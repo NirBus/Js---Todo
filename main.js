83abf883b91e3b154aac2d0d2da001a98cb8a3b2
VenDiago <alizg@code.edu.az>
2022-04-28 01:23:59 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onload= function () {
+    const button = document.getElementsByClassName("button");
+
+
+    button[0].onclick=function () {
+        document.getElementById("click-1").innerHTML="First todo item"
+        document.getElementById("click-2").innerHTML="Other todo item"
+        document.getElementById("click-3").innerHTML="And this is another one :)"
+    }
+
+
+
+
+
+}
